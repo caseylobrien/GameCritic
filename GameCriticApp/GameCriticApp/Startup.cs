08b17f4f9b2b38f3dba02108b094ea60a998944f
caseylobrien <caseyobrien2014@gmail.com>
2017-02-08 11:27:39 -0500

    Poject Init

---
+++ GameCriticApp/GameCriticApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameCriticApp.Startup))]
+namespace GameCriticApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
